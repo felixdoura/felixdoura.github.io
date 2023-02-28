0601aa7f0f6df435b3d457a270d125b5ba4d225c
Felix Doura <117871274+felixdoura@users.noreply.github.com>
2023-02-27 23:19:58 -0300

    Delete routes directory

--- routes/skillsRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = require('path');
-
-const skillsController = require('../controllers/skillsController');
-
-router.get('/', skillsController.index);  
-
-module.exports = router;
