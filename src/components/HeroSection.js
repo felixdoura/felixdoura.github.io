988f42e78bfe5283cdba1f0aea672e78a4e51d4a
Felix D <felixdoura@hotmail.com>
2023-07-05 14:36:26 -0300

    cambios del deploy

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../App.css';
-import { Button } from './Button';
 import './HeroSection.css';
 import { BsDiscord } from 'react-icons/bs'
 
@@ -9,16 +8,6 @@ function HeroSection() {
         <div className='hero-container'>
             {/* <video src='/videos/video-solar.mp4' autoPlay loop muted /> */}
             <div className='hero-btns'>
-                <Button
-                    className='button'
-                    buttonStyle='button-outline'
-                    buttonSize='button-large'
-                >
-                    <a href='/' className='button-link'>
-                        CONOZCANOS
-                    </a>
-                </Button>
-
                 <a
                     href='https://discord.gg/Km899xDQWV'
                     className='button discord-link'
